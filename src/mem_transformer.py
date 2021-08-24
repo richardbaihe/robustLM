fix bug of adaptive softmax div_val with DDP

src/mem_transformer.py
@@ -35,6 +35,9 @@ class PositionalEmbedding(nn.Module):
         self.register_buffer('inv_freq', inv_freq)
 
     def forward(self, pos_seq, bsz=None, sega=False):
+        if torch.is_autocast_enabled():
+            self.inv_freq = self.inv_freq.half()
+            pos_seq=pos_seq.half()
         if sega:
             sinusoid_inp = torch.bmm(pos_seq.unsqueeze(-1),
                                      self.inv_freq.unsqueeze(0).unsqueeze(0).expand(pos_seq.size(0), -1, -1))
@@ -675,6 +678,8 @@ class AdaptiveEmbedding(nn.Module):
                 self.emb_layers.append(nn.Embedding(r_idx-l_idx, d_emb_i))
                 self.emb_projs.append(nn.Parameter(
                     torch.Tensor(d_proj, d_emb_i)))
+        self.register_buffer("_float_tensor", torch.FloatTensor(1))
+        self.register_buffer("_half_tensor", torch.HalfTensor(1))
 
     def forward(self, inp):
         if self.div_val == 1:
@@ -682,30 +687,55 @@ class AdaptiveEmbedding(nn.Module):
             if self.d_proj != self.d_embed:
                 embed = F.linear(embed, self.emb_projs[0])
         else:
-            param = next(self.parameters())
             inp_flat = inp.reshape(-1)
-            emb_flat = torch.zeros([inp_flat.size(0), self.d_proj],
-                                   dtype=param.dtype, device=param.device)
+            if torch.is_autocast_enabled():
+                emb_flat = self._half_tensor.new(inp_flat.shape + (self.d_proj,))
+            else:
+                emb_flat = self._float_tensor.new(inp_flat.shape + (self.d_proj,))
+
             for i in range(len(self.cutoffs)):
-                l_idx, r_idx = self.cutoff_ends[i], self.cutoff_ends[i + 1]
-
-                mask_i = (inp_flat >= l_idx) & (inp_flat < r_idx)
-                indices_i = mask_i.nonzero().squeeze()
-
-                if indices_i.numel() == 0:
-                    continue
-
-                inp_i = inp_flat.index_select(0, indices_i) - l_idx
-                emb_i = self.emb_layers[i](inp_i)
-                emb_i = F.linear(emb_i, self.emb_projs[i])
-
-                emb_flat.index_copy_(0, indices_i, emb_i)
-
+                mask = inp_flat.lt(self.cutoffs[i])
+                if i > 0:
+                    mask.mul_(inp_flat.ge(self.cutoffs[i - 1]))
+                    chunk_input = inp_flat[mask] - self.cutoffs[i - 1]
+                else:
+                    chunk_input = inp_flat[mask]
+                if mask.any():
+                    emb_i = self.emb_layers[i](chunk_input)
+                    emb_flat[mask]=F.linear(emb_i, self.emb_projs[i])
             embed = emb_flat.view(*inp.size(), self.d_proj)
-
         embed.mul_(self.emb_scale)
-
         return embed
+    # def forward(self, inp):
+    #     if self.div_val == 1:
+    #         embed = self.emb_layers[0](inp)
+    #         if self.d_proj != self.d_embed:
+    #             embed = F.linear(embed, self.emb_projs[0])
+    #     else:
+    #         param = next(self.parameters())
+    #         inp_flat = inp.reshape(-1)
+    #         emb_flat = torch.zeros([inp_flat.size(0), self.d_proj],
+    #                                dtype=torch.float16 if torch.is_autocast_enabled() else param.dtype, device=param.device)
+    #         for i in range(len(self.cutoffs)):
+    #             l_idx, r_idx = self.cutoff_ends[i], self.cutoff_ends[i + 1]
+
+    #             mask_i = (inp_flat >= l_idx) & (inp_flat < r_idx)
+    #             indices_i = mask_i.nonzero().squeeze()
+
+    #             if indices_i.numel() == 0:
+    #                 continue
+
+    #             inp_i = inp_flat.index_select(0, indices_i) - l_idx
+    #             emb_i = self.emb_layers[i](inp_i)
+    #             emb_i = F.linear(emb_i, self.emb_projs[i])
+
+    #             emb_flat.index_copy_(0, indices_i, emb_i)
+
+    #         embed = emb_flat.view(*inp.size(), self.d_proj)
+
+    #     embed.mul_(self.emb_scale)
+
+    #     return embed
 
 
 def get_ith_embed_layer_given_index(index, word_emb):
@@ -725,7 +755,8 @@ class MemTransformerLM(nn.Module):
                  cutoffs=[], adapt_inp=False,
                  same_length=False, attn_type=0, clamp_len=-1,
                  sample_softmax=-1, cl_all_root_index=None, cl_all_leaf_index=None,
-                 adaptive_class_softmax=False, cl_root_leaf_dict=None, word2class_id=None):
+                 adaptive_class_softmax=False, cl_root_leaf_dict=None, word2class_id=None,
+                 mix_vocab=True):
         super(MemTransformerLM, self).__init__()
         # if word2class_id:
         #     n_token = n_token-len(cl_root_leaf_dict)
@@ -746,10 +777,10 @@ class MemTransformerLM(nn.Module):
         # else:
         self.word_emb = AdaptiveEmbedding(n_token, d_embed, d_model, cutoffs,
                                         div_val=div_val)
-            
-        self.auxiliary_projection_layer = nn.Linear(d_model, d_model)
-        self.auxiliary_output_layer = nn.Linear(
-            d_model, len(cl_all_root_index))
+        if not mix_vocab:
+            self.auxiliary_projection_layer = nn.Linear(d_model, d_model)
+            self.auxiliary_output_layer = nn.Linear(
+                d_model, len(cl_all_root_index))
         self.cl_root_vocab = {}
         self.drop = nn.Dropout(dropout)
 

src/train.py
@@ -19,10 +19,10 @@ import torch.nn as nn
 import torch.optim as optim
 from fp16 import FP16_Module
 from fp16 import FP16_Optimizer
+from torch.cuda.amp import autocast
 from torch.nn.parallel.distributed import DistributedDataParallel as torchDDP
 from apex.parallel import DistributedDataParallel as apexDDP
 from apex.optimizers import FusedAdam as Adam
-from apex import amp, optimizers
 from apex import amp, optimizers
 from utils.arguments import get_args
 from data_utils import MixCorpus
@@ -194,7 +194,7 @@ def get_model(args):
                 ext_len=args.ext_len, mem_len=args.mem_len, cutoffs=cutoffs,
                 same_length=args.same_length, attn_type=args.attn_type,
                 clamp_len=args.clamp_len, sample_softmax=args.sample_softmax, 
-                                     cl_all_root_index=args.cl_all_root_index, cl_all_leaf_index=args.cl_all_leaf_index, adaptive_class_softmax=args.adaptive_class_softmax, cl_root_leaf_dict=args.cl_root_leaf_dict, word2class_id=args.word2class_id)
+                                     cl_all_root_index=args.cl_all_root_index, cl_all_leaf_index=args.cl_all_leaf_index, adaptive_class_softmax=args.adaptive_class_softmax, cl_root_leaf_dict=args.cl_root_leaf_dict, word2class_id=args.word2class_id,mix_vocab=args.mix_vocab)
         model.apply(weights_init)
         model.word_emb.apply(weights_init) # ensure embedding init is not overridden by out_layer in case of weight sharing
         # if args.learn_offset:
@@ -245,14 +245,15 @@ def setup_model_and_optimizer(args):
     if args.load_lastest:
         args.iteration = load_checkpoint(model, optimizer, scheduler, args, best=False)
     
-    if args.fp16:
-        # model = model.half()
-        model, optimizer = amp.initialize(model, optimizer,
-                                      opt_level="O2",
-                                      loss_scale='dynamic'
-                                      )
-    model = apexDDP(model)
-    return model, optimizer, scheduler
+    # if args.fp16:
+    #     model, optimizer = amp.initialize(model, optimizer,
+    #                                   opt_level="O2",
+    #                                   loss_scale='dynamic'
+    #                                   )
+    # model = apexDDP(model)
+    scaler = torch.cuda.amp.GradScaler(enabled=args.fp16)
+    model = torchDDP(model, device_ids=[args.rank])
+    return model, optimizer, scheduler, scaler
 
 def get_batch(data_iterator, iteration):
     data = data_iterator.get_batch(iteration)
@@ -306,7 +307,7 @@ def forward_step(data_iterator, model, mems, iteration, args):
     root_mask = cl_target!=target
     input_root = args.input_root and class_prediction
 
-    hypernym_input = cl_data*(cl_data != data)
+    hypernym_input = cl_data*(cl_data != data) if args.learn_offset else None
     if class_prediction:
         if input_root:
             ret = model(cl_data, target, cl_target, mems, args, class_prediction=True)
@@ -329,36 +330,46 @@ def forward_step(data_iterator, model, mems, iteration, args):
     non_cl_loss = lm_loss[~root_mask]
     return lm_loss, auxiliary_loss, cl_loss, non_cl_loss, new_mems
 
-def backward_step(optimizer, model, lm_loss, auxiliary_loss, args):
+def backward_step(optimizer, model, scaler, lm_loss, auxiliary_loss, args):
     if args.multi_obj:
         loss = 0.8*lm_loss + 0.2*auxiliary_loss
     else:
         loss = lm_loss
     optimizer.zero_grad()
-    if args.fp16:
-        with amp.scale_loss(loss, optimizer) as scaled_loss:
-            scaled_loss.backward()
-        # optimizer.backward(loss, update_master_grads=False)
-    else:
-        loss.backward()
+
+    scaler.scale(loss).backward()
+    scaler.unscale_(optimizer)
+    torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip)
+    # for name, param in model.named_parameters():
+    #     if param.grad is None:
+    #         print(name)
+    scaler.step(optimizer)
+    scaler.update()
+    # if args.fp16:
+    #     with amp.scale_loss(loss, optimizer) as scaled_loss:
+    #         scaled_loss.backward()
+    # else:
+    #     loss.backward()
     
     lm_loss_reduced = get_reduced_loss(lm_loss, args.world_size)
     auxiliary_loss_reduced = get_reduced_loss(auxiliary_loss, args.world_size)
 
-    if args.fp16:
-        torch.nn.utils.clip_grad_norm_(amp.master_params(optimizer), args.clip)
-    else:
-        torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip)
+    # if args.fp16:
+    #     torch.nn.utils.clip_grad_norm_(amp.master_params(optimizer), args.clip)
+    # else:
+    #     torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip)
 
     return lm_loss_reduced, auxiliary_loss_reduced
 
-def train_step(data_iterator, mems, model, optimizer, lr_scheduler, iteration, args):
+def train_step(data_iterator, mems, model, optimizer, lr_scheduler, scaler, iteration, args):
+    with autocast(enabled=args.fp16):
 
-    lm_loss, auxiliary_loss, cl_loss, non_cl_loss, new_mems = forward_step(data_iterator, model, mems, iteration, args)
-    lm_loss = lm_loss.float().mean().type_as(lm_loss)
-    auxiliary_loss = auxiliary_loss.float().mean().type_as(auxiliary_loss)
-    lm_loss_reduced, auxiliary_loss_reduced = backward_step(optimizer, model, lm_loss, auxiliary_loss, args)
-    optimizer.step()
+        lm_loss, auxiliary_loss, cl_loss, non_cl_loss, new_mems = forward_step(data_iterator, model, mems, iteration, args)
+        lm_loss = lm_loss.mean().type_as(lm_loss)
+        auxiliary_loss = auxiliary_loss.mean().type_as(auxiliary_loss)
+
+    lm_loss_reduced, auxiliary_loss_reduced = backward_step(optimizer, model, scaler, lm_loss, auxiliary_loss, args)
+    # optimizer.step()
 
     if iteration < args.warmup_step:
         curr_lr = args.lr * iteration / args.warmup_step
@@ -373,7 +384,7 @@ def train_step(data_iterator, mems, model, optimizer, lr_scheduler, iteration, a
 
     return lm_loss_reduced, auxiliary_loss_reduced, cl_lm_loss_reduced, non_cl_lm_loss_reduced, new_mems
 
-def train(model, optimizer, lr_scheduler, corpus, train_data_iterator, val_data_iterator, args):
+def train(model, optimizer, lr_scheduler,scaler, corpus, train_data_iterator, val_data_iterator, args):
     # Turn on training mode which enables dropout.
     # global train_step, train_loss, best_val_loss, eval_start_time, log_start_time
     model.train()
@@ -425,7 +436,7 @@ def train(model, optimizer, lr_scheduler, corpus, train_data_iterator, val_data_
         # if hypernym_grad and args.learn_offset and iteration>args.cl_steps:
         #     model.module.change()
         #     hypernym_grad = False
-        lm_loss, auxiliary_loss, cl_loss, non_cl_loss, mems = train_step(train_data_iterator, mems, model, optimizer, lr_scheduler, iteration, args)
+        lm_loss, auxiliary_loss, cl_loss, non_cl_loss, mems = train_step(train_data_iterator, mems, model, optimizer, lr_scheduler, scaler, iteration, args)
         iteration += 1
         current_lm_loss = lm_loss.data.detach().float().item()
         total_lm_loss += current_lm_loss
@@ -652,7 +663,7 @@ def main():
 
     torch.distributed.barrier()
 
-    model, optimizer, scheduler = setup_model_and_optimizer(args)
+    model, optimizer, scheduler, scaler = setup_model_and_optimizer(args)
 
     args.eval_batch_size = 10
     tr_iter, va_iter, te_iter = None, None, None
@@ -664,7 +675,7 @@ def main():
         device=device, ext_len=args.ext_len,rank=0, world_size=1)
 
     if args.do_train:
-        train(model, optimizer, scheduler, corpus, tr_iter, va_iter, args)
+        train(model, optimizer, scheduler, scaler, corpus, tr_iter, va_iter, args)
     if args.do_eval and args.checkpoint_dir:
         for checkpoint_name in glob.glob(os.path.join(args.checkpoint_dir,'iter_*/model_optim_rng.pt')):
             iteration = load_checkpoint(model, optimizer, scheduler, args, checkpoint_name=checkpoint_name)